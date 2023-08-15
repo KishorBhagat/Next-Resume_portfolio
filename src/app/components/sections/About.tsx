6497acd099cca8617b182044e9be2d72812f44da
Kishor Bhagat <kishorebhagat663@gmail.com>
2023-08-16 01:15:57 +0530

    fixing deployment error

--- src/app/components/sections/About.tsx
+++ src/app/components/sections/About.tsx
@@ -5,7 +5,7 @@ export default function About() {
     <section className={styles.about} id='about'>
       <h2 className={styles.heading}>ABOUT</h2>
       <p>Hello! My name is Kishor Bhagat, a Computer Science and Engineering undergraduate from Veer Surendra Sai University of technology, Burla. A coding enthusiast and a passionate learner.</p>
-      <p>I started learning to code in high school, and my interest in web development began to flourish as soon as I entered college. Eager to enhance my skills, I joined the coding club of my college, immersing myself in various projects. Among these, I contributed to creating websites for college techno-cultural fests, developed interfaces for coding contests, and collaboratively worked in the club's own projects.</p>
+      <p>I started learning to code in high school, and my interest in web development began to flourish as soon as I entered college. Eager to enhance my skills, I joined the coding club of my college, immersing myself in various projects. Among these, I contributed to creating websites for college techno-cultural fests, developed interfaces for coding contests, and collaboratively worked in the club&apos;s own projects.</p>
       <p>Currently, my primary focus is on building a diverse portfolio of projects that showcase my abilities and reflect my growth as a developer. I am continuously trying to seek new opportunities that challenge me and help me expand my knowledge and expertise in the field.</p>
     </section>
   )
